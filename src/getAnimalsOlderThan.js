ce68991f5738dafb3faf6aee49ac6808b66a6ea8
fernandoallisson <fernando.alisson@ufpe.br>
2023-05-12 00:20:14 -0300

    Resolution the 2nd task

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,9 @@
 const data = require('../data/zoo_data');
 
-const getAnimalsOlderThan = (animal, age) => {
-  // seu código aqui
+const getAnimalsOlderThan = (animal, idade) => {
+  const animais = data.species.find(({ name }) => name === animal);
+  const verificacao = animais.residents.every(({ age }) => age >= idade);
+  return verificacao;
 };
 
 module.exports = getAnimalsOlderThan;
