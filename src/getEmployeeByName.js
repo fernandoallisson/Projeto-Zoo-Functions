46a1c23fc122ed190246a4ec011ba9ae3e170e7b
fernandoallisson <fernando.alisson@ufpe.br>
2023-05-12 16:55:39 -0300

    Resolution error the 3st task

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -4,7 +4,7 @@ const getEmployeeByName = (employeeName) => {
   const matchingEmployers = data.employees.filter(({ firstName, lastName }) =>
     firstName === employeeName || lastName === employeeName);
   if (matchingEmployers.length === 0) {
-    return [];
+    return {};
   } return matchingEmployers;
 };
 
