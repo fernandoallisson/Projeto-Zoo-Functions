dab476e1f7ebf07cb5cfb649e2aad284954f54d5
fernandoallisson <fernando.alisson@ufpe.br>
2023-05-11 23:21:35 -0300

    Resolution the 1st task

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
-const getSpeciesByIds = (ids) => {
-  // seu código aqui
+const getSpeciesByIds = (...ids) => {
+  if (ids.length === 0) {
+    return [];
+  }
+  return (data.species
+    .filter(({ id }) => ids
+      .some((elem) => elem === id)));
 };
 
 module.exports = getSpeciesByIds;
