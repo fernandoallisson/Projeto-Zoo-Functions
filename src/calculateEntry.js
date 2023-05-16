d84fa7d5dccf367ced1d87a3bccac958cd9070c1
fernandoallisson <fernando.alisson@ufpe.br>
2023-05-16 19:42:44 -0300

    Resolution of the 8th task

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,32 @@
 const data = require('../data/zoo_data');
 
-const countEntrants = (entrants) => {
-  // seu código aqui
+const countEntrants = (array) => {
+  let crianca = 0;
+  let adulto = 0;
+  let senhor = 0;
+
+  array.forEach((element) => {
+    if (element.age < 18) {
+      crianca += 1;
+    } else if (element.age < 50) {
+      adulto += 1;
+    } else {
+      senhor += 1;
+    }
+  });
+
+  return { child: crianca, adult: adulto, senior: senhor };
 };
 
-const calculateEntry = (entrants) => {
-  // seu código aqui
+const calculateEntry = (...entrants) => {
+  if (entrants[0] === undefined) {
+    return 0;
+  }
+  const entrada = countEntrants(...entrants);
+  const valorChild = data.prices.child;
+  const valorAdult = data.prices.adult;
+  const valorSenior = data.prices.senior;
+  return entrada.child * valorChild + entrada.adult * valorAdult + entrada.senior * valorSenior;
 };
 
 module.exports = { calculateEntry, countEntrants };
