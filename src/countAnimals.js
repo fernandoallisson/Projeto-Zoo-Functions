74c9e1fab25fbda47eb7432f98118fb1be23785d
fernandoallisson <fernando.alisson@ufpe.br>
2023-05-16 16:34:08 -0300

    Resolution of the 5th task

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,27 @@
 const data = require('../data/zoo_data');
 
-const countAnimals = (animal) => {
-  // seu código aqui
+// Essa função retorna apena o número de espécie de um determinado objeto.
+const contadorDeAnimaisPorEspecies = (...species) => {
+  const paramentroSexo = species[0].sex;
+  const paramentroSpec = species[0].species;
+  const ObjAnimal = data.species.filter(({ name }) => name === paramentroSpec)[0];
+  const contarSex = ObjAnimal.residents.filter(({ sex }) => sex === paramentroSexo);
+
+  if (paramentroSexo) {
+    return contarSex.length;
+  }
+  return ObjAnimal.residents.length;
+};
+
+const countAnimals = (...animal) => {
+  if (animal[0] === undefined) {
+    const animaisFiltrados = data.species.reduce((obje, { name, residents }) => {
+      const obj = { ...obje };
+      obj[name] = residents.length;
+      return obj;
+    }, {});
+    return animaisFiltrados;
+  } return contadorDeAnimaisPorEspecies(...animal);
 };
 
 module.exports = countAnimals;
